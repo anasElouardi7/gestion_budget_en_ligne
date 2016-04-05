50332fcc12484d754b8e0339fc8df48263a242e1
lenovo <lenovo@192.168.1.2>
2016-04-05 18:10:31 +0100

    creation du projet

---
+++ src/java/bean/User.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author hamid
+ */
+@Entity
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private String tel;
+    private String email;
+    private int type; // que ca soit un {chefDepartement , doyen , president ...}
+    @OneToOne
+    private EntiteAdministratif entiteAdministratif; // doyen matalane howa chef d idara o chef de filiere GC
+    @ManyToOne
+    private Faculte faculte;
+
+    //**************************************************************
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public EntiteAdministratif getEntiteAdministratif() {
+        return entiteAdministratif;
+    }
+
+    public void setEntiteAdministratif(EntiteAdministratif entiteAdministratif) {
+        this.entiteAdministratif = entiteAdministratif;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public Faculte getFaculte() {
+        if (faculte == null) {
+            faculte = new Faculte();
+        }
+        return faculte;
+    }
+
+    public void setFaculte(Faculte faculte) {
+        this.faculte = faculte;
+    }
+
+    //****************************************************************
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "bean.User[ id=" + id + " ]";
+    }
+
+}
